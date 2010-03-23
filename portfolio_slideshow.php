800085bbfcf3778db581cdd8081cc9701923dd16
daltonrooney <daltonrooney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-23 19:48:29 +0000

    tagging version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/portfolio-slideshow/tags/0.3@221016 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- portfolio_slideshow.php
+++ portfolio_slideshow.php
@@ -4,7 +4,7 @@ Plugin Name: Portfolio Slideshow
 Plugin URI: http://daltonrooney.com/portfolio
 Description: A shortcode that inserts a clean and simple jQuery + cycle powered slideshow of all image attachments on a post or page. Use shortcode [portfolio_slideshow] to activate.
 Author: Dalton Rooney
-Version: 0.2
+Version: 0.3
 Author URI: http://daltonrooney.com
 */ 
 
@@ -18,28 +18,53 @@ function portfolio_shortcode($attr) {
 	</div> 
 	
 	<div class="portfolio-slideshow">';
-					
-	$args = array(
+	
+	$portfolio_slideshow_size = get_option('portfolio_slideshow_size'); //loads the size option from the options table, if it's set
+	
+	if (!empty($portfolio_slideshow_size) ) { // if we've set the size
+	$args =  array(
 		'order'          => 'ASC',
-		'orderby' 		 => 'menu_order',
+		'orderby' 		 => 'menu_order ID',
 		'post_type'      => 'attachment',
 		'post_parent'    => get_the_ID(),
 		'post_mime_type' => 'image',
 		'post_status'    => null,
 		'numberposts'    => -1,
-		'size'			 => 'large' //Change this to specify a different image size. The options are thumbnail, medium, large, or full.
-	);
+		'size'			 => $portfolio_slideshow_size
+	) ;	
+	
+	} else {	// else use the default 'large'
+	$args = array(			
+		'order'          => 'ASC',
+		'orderby' 		 => 'menu_order ID',
+		'post_type'      => 'attachment',
+		'post_parent'    => get_the_ID(),
+		'post_mime_type' => 'image',
+		'post_status'    => null,
+		'numberposts'    => -1,
+		'size'			 => 'large'
+	); }
 	
 	$attachments = get_posts($args);
 	if ($attachments) {
 		foreach ($attachments as $attachment) {
-		echo "<div class='slideshow-next'>";
+			echo "<div class='slideshow-next'>";
+			
 			echo wp_get_attachment_image($attachment->ID, $args['size'], false, false);
-		$caption = $attachment->post_excerpt;
-		if (isset($caption)) { echo '<p class="slideshow-caption">'.$caption.'</p>'; }
-		echo "</div>";
-		}
-	}
+			
+			$title = $attachment->post_title;
+			if (isset($title)) { 
+				echo '<p class="slideshow-title">'.$title.'</p>'; 
+			}	
+			
+			$caption = $attachment->post_excerpt;
+			if (isset($caption)) { 
+				echo '<p class="slideshow-caption">'.$caption.'</p>'; 
+			}
+			
+			echo "</div>";
+		} // end slideshow loop
+	} // end if ($attachments)
 	
 	echo "</div><!-- ends the portfolio-slideshow div-->";
 
@@ -92,4 +117,58 @@ function portfolio_head() {
 } // ends the javascript & css for head
 
 add_action('wp_head', 'portfolio_head');
-?>
+
+
+
+
+
+//let's create an admin menu now
+
+// Hook in the action for the admin options page
+add_action('admin_menu', 'add_portfolio_slideshow_option_page');
+
+function add_portfolio_slideshow_option_page() {
+	// Hook in the options page function
+	add_options_page('Portfolio Slideshow', 'Portfolio Slideshow', 6, __FILE__, 'portfolio_slideshow_options_page');
+}
+
+
+function portfolio_slideshow_options_page() {
+
+// Output a simple options page ?>
+
+
+<div class="wrap" style="width:500px">
+<h2>Portfolio Slideshow Options</h2>
+<form method="post" action="options.php">
+
+<?php wp_nonce_field('update-options'); ?>
+
+<table class="form-table">
+<tr valign="top">
+<th scope="row">Slideshow Size</th>
+<td><select name="portfolio_slideshow_size" value="<?php echo get_option('portfolio_slideshow_size'); ?>" />
+<option value="thumbnail" <?php if(get_option(portfolio_slideshow_size) == thumbnail) echo " selected='selected'";?>>Thumbnail</option>
+<option value="medium" <?php if(get_option(portfolio_slideshow_size) == medium) echo " selected='selected'";?>>Medium</option>
+<option value="large" <?php if(get_option(portfolio_slideshow_size) == large) echo " selected='selected'";?>>Large</option>
+<option value="full" <?php if(get_option(portfolio_slideshow_size) == full) echo " selected='selected'";?>>Full</option>
+</select>
+</td>
+</tr>	
+</table>
+
+<input type="hidden" name="page_options" value="portfolio_slideshow_size" />
+<input type="hidden" name="action" value="update" />	
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+<p>The slideshow size refers to the default image sizes that WordPress creates when you upload an image. You can customize these image sizes in the <a href="<?php bloginfo('wpurl');?>/wp-admin/options-media.php">Media Settings</a> control panel. </p>
+<h2>Support this plugin</h2>
+<p>I don&#8217;t accept donations for this software, but I do have a recommendation for a web host if you&#8217;re interested. I&#8217;ve been using <a href="http://www.a2hosting.com/1107.html">A2 Hosting</a> for years, and they provide fantastic service and support. If you sign up through the link below, I get a referral fee, which helps me maintain this plugin. Their one-click WordPress install will have you up and running in just a couple of minutes.</p> 
+<p><a  href="http://www.a2hosting.com/1107.html"><img style="margin:10px 0;" src="http://daltonrooney.com/portfolio/wp-content/uploads/2010/01/green_234x60.jpeg" alt="" title="green_234x60" width="234" height="60" class="alignnone size-full wp-image-148" /></a></p>
+
+</div>
+
+
+<?php } ?>
