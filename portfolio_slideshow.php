ee0aa5fc415cee7ca5b7d21332c6b77e8cd4cf95
daltonrooney <daltonrooney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-10 19:53:04 +0000

    update version number
    git-svn-id: https://plugins.svn.wordpress.org/portfolio-slideshow/trunk@287872 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- portfolio_slideshow.php
+++ portfolio_slideshow.php
@@ -4,7 +4,7 @@ Plugin Name: Portfolio Slideshow
 Plugin URI: http://daltonrooney.com/portfolio
 Description: A shortcode that inserts a clean and simple jQuery + cycle powered slideshow of all image attachments on a post or page. Use shortcode [portfolio_slideshow] to activate.
 Author: Dalton Rooney
-Version: 0.5b
+Version: 0.5.0
 Author URI: http://daltonrooney.com
 */ 
 
